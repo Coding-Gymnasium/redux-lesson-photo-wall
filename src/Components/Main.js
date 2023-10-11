2548c222e5d22fabb121f30d937c8ad41120e7fe
Nico Rithner <nicorithner@gmail.com>
2023-10-11 14:58:56 -0600

    Get data using componentDidMount

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,27 +7,7 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      posts: [
-        {
-          id: "0",
-          description: "beautiful landscape",
-          imageLink:
-            "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-            "3919321_1443393332_n.jpg",
-        },
-        {
-          id: "1",
-          description: "Aliens???",
-          imageLink:
-            "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
-        },
-        {
-          id: "2",
-          description: "On a vacation!",
-          imageLink:
-            "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
-        },
-      ],
+      posts: [],
     };
     this.removePhoto = this.removePhoto.bind(this);
   }
@@ -36,6 +16,13 @@ class Main extends Component {
     this.setState((state) => ({
       posts: state.posts.filter((post) => post !== postRemoved),
     }));
+  }
+
+  componentDidMount() {
+    const data = SimulateFetchFromDatabase();
+    this.setState({
+      posts: data,
+    });
   }
 
   render() {
@@ -47,4 +34,28 @@ class Main extends Component {
     );
   }
 }
+
+function SimulateFetchFromDatabase() {
+  return [
+    {
+      id: "0",
+      description: "beautiful landscape",
+      imageLink:
+        "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+        "3919321_1443393332_n.jpg",
+    },
+    {
+      id: "1",
+      description: "Aliens???",
+      imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
+    },
+    {
+      id: "2",
+      description: "On a vacation!",
+      imageLink:
+        "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
+    },
+  ];
+}
+
 export default Main;
