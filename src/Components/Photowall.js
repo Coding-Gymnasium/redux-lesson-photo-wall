84119a59ae9e23592ab6e12f146c92316b1f5f43
Nico Rithner <nicorithner@gmail.com>
2023-10-11 11:08:48 -0600

    Implement Remove button

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -5,7 +5,9 @@ function PhotoWall(props) {
   return (
     <div className="photoGrid">
       {props.posts.map((post, index) => {
-        return <Photo key={index} post={post} />;
+        return (
+          <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
+        );
       })}
     </div>
   );
