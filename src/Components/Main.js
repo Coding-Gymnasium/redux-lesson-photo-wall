a5a59922ec1de5c54a37b605e0efa7beb821e82c
Nico Rithner <nicorithner@gmail.com>
2023-10-12 09:53:45 -0600

    Roll back to array source. Add navigate function

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+import AddPhoto from "./AddPhoto.js";
 import Photowall from "./Photowall.js";
 import Title from "./Title";
 
@@ -7,9 +8,31 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      posts: [],
+      posts: [
+        {
+          id: "0",
+          description: "beautiful landscape",
+          imageLink:
+            "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+            "3919321_1443393332_n.jpg",
+        },
+        {
+          id: "1",
+          description: "Aliens???",
+          imageLink:
+            "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
+        },
+        {
+          id: "2",
+          description: "On a vacation!",
+          imageLink:
+            "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
+        },
+      ],
+      screen: "photos", // photos, addPhoto
     };
     this.removePhoto = this.removePhoto.bind(this);
+    this.navigate = this.navigate.bind(this);
   }
 
   removePhoto(postRemoved) {
@@ -18,44 +41,30 @@ class Main extends Component {
     }));
   }
 
-  componentDidMount() {
-    const data = SimulateFetchFromDatabase();
-    this.setState({
-      posts: data,
-    });
+  navigate() {
+    this.setState({ screen: "addPhoto" });
   }
 
   render() {
     return (
-      <div>
-        <Title title={"Photowall"} />
-        <Photowall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
-      </div>
+      <>
+        {this.state.screen === "photos" && (
+          <div>
+            <Title title={"Photowall"} />
+            <Photowall
+              posts={this.state.posts}
+              onRemovePhoto={this.removePhoto}
+              onNavigate={this.navigate}
+            />
+          </div>
+        )}
+        {this.state.screen === "addPhoto" && (
+          <div>
+            <AddPhoto />
+          </div>
+        )}
+      </>
     );
   }
 }
-
-function SimulateFetchFromDatabase() {
-  return [
-    {
-      id: "0",
-      description: "beautiful landscape",
-      imageLink:
-        "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-        "3919321_1443393332_n.jpg",
-    },
-    {
-      id: "1",
-      description: "Aliens???",
-      imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg",
-    },
-    {
-      id: "2",
-      description: "On a vacation!",
-      imageLink:
-        "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
-    },
-  ];
-}
-
 export default Main;
