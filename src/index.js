510261b3c4a25cb266f608a3984ed5c1be26fefc
Nico Rithner <nicorithner@gmail.com>
2023-10-12 11:15:18 -0600

    Setup routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import ReactDOM from "react-dom/client";
 import "./styles/stylesheet.css";
 import Main from "./Components/Main";
+import AddPhoto from "./Components/AddPhoto.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Main />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/add-photo" element={<AddPhoto />} />
+    </Routes>
+  </Router>,
+);
