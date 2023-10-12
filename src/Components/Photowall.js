bcb1e489661a3a6a28917e2326be44d4bf4fc560
Nico Rithner <nicorithner@gmail.com>
2023-10-12 10:04:42 -0600

    Wrap button on link tag

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,22 +1,36 @@
 import PropTypes from "prop-types";
 import React from "react";
-
 import Photo from "./Photo.js";
 
 function PhotoWall(props) {
   return (
-    <div className="photoGrid">
-      {props.posts.map((post, index) => {
-      return (<Photo key = {index} post = {post} onRemovePhoto =
-               { props.onRemovePhoto } />
-        );
-      })}
-    </div>);
+    <>
+      <a onClick={props.onNavigate} href="#AddPhoto">
+        <div className="addIcon">
+          <div className="cross-vertical">
+            <div className="cross-horizontal"></div>
+          </div>
+        </div>
+      </a>
+
+      <div className="photoGrid">
+        {props.posts.map((post, index) => {
+          return (
+            <Photo
+              key={index}
+              post={post}
+              onRemovePhoto={props.onRemovePhoto}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 PhotoWall.propTypes = {
-      posts: PropTypes.array.isRequired,
-      onRemovePhoto: PropTypes.func.isRequired 
-}
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired,
+};
 
 export default PhotoWall;
