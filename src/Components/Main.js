bbd4503bc778b5a564d3af9cc1f19395ee6532a5
Nico Rithner <nicorithner@gmail.com>
2023-10-13 15:10:39 -0600

    Refactor to use componentDidMount and action

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import Photowall from "./Photowall.js";
 import Title from "./Title";
+import { removePost } from "../redux/actions.js";
 
 class Main extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.props.removePost(1);
+  }
+
   render() {
     return (
       <div>
         <Title title={"Photowall"} />
-         <Photowall posts={this.props.posts} />
+        <Photowall {...this.props} />
       </div>
     );
   }
