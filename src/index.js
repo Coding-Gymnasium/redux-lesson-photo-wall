31e89754dbf9dcdc3233ee6746c6db86016671de
Nico Rithner <nicorithner@gmail.com>
2023-10-13 09:47:11 -0600

    Create root reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import "./styles/stylesheet.css";
 import Main from "./Components/Main";
 import AddPhoto from "./Components/AddPhoto.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducer.js";
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
