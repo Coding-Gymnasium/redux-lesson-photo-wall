0086d53cb2d1120d100d76a330dfc514d5c701e8
Nico Rithner <nicorithner@gmail.com>
2023-10-13 15:09:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import ReactDOM from "react-dom/client";
 import "./styles/stylesheet.css";
-import Main from "./Components/Main";
-import AddPhoto from "./Components/AddPhoto.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./Components/App.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./redux/reducer.js";
+import AddPhoto from "./Components/AddPhoto.js";
 
 const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/add-photo" element={<AddPhoto />} />
-    </Routes>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/add-photo" element={<AddPhoto />} />
+      </Routes>
+    </Router>
+  </Provider>,
 );
