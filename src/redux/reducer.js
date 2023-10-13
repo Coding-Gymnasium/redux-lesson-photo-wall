32e35d09e9add0a55aadd2e732b98a544546dc21
Nico Rithner <nicorithner@gmail.com>
2023-10-13 10:06:30 -0600

    Create data directory with posts

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,7 @@
-const postReducer = function posts(state, action) {
-  return state
-}
+import posts from "../data/post.js";
+
+const postReducer = function posts(state = posts, action) {
+  return state;
+};
 
 export default postReducer;
